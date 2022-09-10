4d96b701c3a079994e8921c8755e22fe9dd089f6
Jayad Arshad <jayadusama@hotmail.co.uk>
2022-09-10 15:11:56 +0100

    main menu

--- index.js
+++ index.js
@@ -18,3 +18,56 @@ connection.connect((err) => {
   console.log("connected as id " + connection.threadId);
   mainMenu();
 });
+
+// Main Menu
+function mainMenu() {
+  inquirer
+    .prompt([
+      //Prompt the user
+      {
+        type: "list",
+        name: "menu",
+        message: "choose an option from the list?",
+        choices: [
+          "View Departments",
+          "View Roles",
+          "View Employees",
+          "Add a Department",
+          "Add a Role",
+          "Add an Employee",
+          "Update an Employee Role",
+          "Exit",
+        ],
+      },
+    ])
+    .then((menuChoice) => {
+      switch (menuChoice.menu) {
+        case "View Departments":
+          displayDepartments();
+          break;
+        case "View Roles":
+          displayRoles();
+          break;
+        case "View Employees":
+          displayEmployees();
+          break;
+        case "Add a Department":
+          addDepartment();
+          break;
+        case "Add a Role":
+          addRole();
+          break;
+        case "Add an Employee":
+          addEmployee();
+          break;
+        case "Update an Employee Role":
+          updateEmployee();
+          break;
+        case "Exit":
+          connection.end();
+          break;
+        default:
+          mainMenu();
+      }
+    });
+}
