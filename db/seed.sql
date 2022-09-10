008d703c78726a65ceefa381759a11c025dadd22
Jayad Arshad <jayadusama@hotmail.co.uk>
2022-09-10 15:10:06 +0100

    added seed and schema

--- db/seed.sql
+++ db/seed.sql
@@ -1,28 +1,30 @@
-DROP DATABASE IF EXISTS employees;
-CREATE DATABASE employees;
-USE employees;
+INSERT INTO department (dept_name)
+VALUES 
+    ("Sales"),
+    ("Engineering"),
+    ("Finance"),
+    ("Legal");
 
-CREATE TABLE department (
-    id INT NOT NULL AUTO_INCREMENT,
-    dept_name VARCHAR(30) NOT NULL,
-    PRIMARY KEY (id)
-);
 
-CREATE TABLE emp_role (
-    id INT AUTO_INCREMENT NOT NULL,
-    title VARCHAR(30) NOT NULL,
-    salary DECIMAL(10,0) NOT NULL,
-    department_id INT,
-    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE set NULL,
-    PRIMARY KEY (id)
-);
+INSERT INTO emp_role (title, salary, department_id)
+VALUES
+    ("Sales Agent", 75000.00, 1),
+    ("Sales Lead", 100000.00, 1),
+    ("Software Engineer", 120000.00, 2),
+    ("Lead Engineer", 145000.00, 2),
+    ("Accountant", 75000.00, 3),
+    ("lawyer", 175000.00, 4),
+    ("Lead Attorney", 215000.00, 4);
 
-CREATE TABLE employee (
-    id INT AUTO_INCREMENT NOT NULL,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    emp_role_id INT,
-    manager_id INT NULL,
-    CONSTRAINT fk_emp_role_id FOREIGN KEY (emp_role_id) REFERENCES emp_role(id) ON DELETE set NULL,
-    PRIMARY KEY (id)
-);
+
+INSERT INTO employee (first_name, last_name, emp_role_id, manager_id)
+VALUES
+    ("frank", "bruno", 2, null, 1),
+    ("nina", "mercedes", 2, 1, 1),
+    ("kill", "bill", 2, 1, 1),
+    ("mike", "tyson", 3, 4, 2),
+    ("Harold", "shipmen", 3, 4, 2),
+    ("al", "pacino", 4, null, 2),
+    ("robert", "deniro", 5, null, 3),
+    ("tom", "Hanks", 6, 7, 4),
+    ("Tracy", "Koch", 7, null, 4);
