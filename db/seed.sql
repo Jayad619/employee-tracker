3befe666951286efde40dad0ad46d8f2f39d9d19
Jayad Arshad <jayadusama@hotmail.co.uk>
2022-09-10 22:19:25 +0100

    changing seeds and restarting index

--- db/seed.sql
+++ db/seed.sql
@@ -1,40 +1,22 @@
-INSERT INTO department (dept_name)
-VALUES 
-    ("Sales"),
-    ("Engineering"),
-    ("Finance"),
-    ("Legal");
+INSERT INTO department (name)
+  VALUES ('Legal'),
+  ('Sales'),
+  ('Marketing'),
+  ('Engineering');
 
 
-INSERT INTO emp_role (title, salary, department_id)
-VALUES
-    ("Sales Agent", 75000.00, 1),
-    ("Sales Lead", 100000.00, 1),
-    ("Software Engineer", 120000.00, 2),
-    ("Lead Engineer", 145000.00, 2),
-    ("Accountant", 75000.00, 3),
-    ("lawyer", 175000.00, 4),
-    ("Lead Attorney", 215000.00, 4);
+  INSERT INTO role (title, salary, department_id)
+  VALUES ('manager', 100000,  1),
+  ('sales asssociate', 70000,  2),
+  ('lawyer', 250000,  1),
+  ('slogan guru', 120000,  3),
+  ('developer', 90000000,  4);
 
-
-INSERT INTO employee (first_name, last_name, emp_role_id, manager_id)
-VALUES
-    ('Jayad', ' Arshad', 1, null),
-    ('Bader', 'Munir', 2, 1),
-    ('Amina', 'Hayat', 3, 2),
-    ('Ahmed', 'Shan', 3, 2),
-    ('Tiah', 'Watt', 4, 5),
-    ('Rez', 'Malek', 5, null),
-    ('Usama', 'Arshad', 4, 5),
-    ('Sponge', 'Bob', 6, null),
-    ('Patrick', "starr", 7, null),
-    ('Skye', 'Jackson', 8, null),
-    ('Samuel', 'Jackson', 9, 8),
-    ('Bob', 'Marley', 9, 8),
-    ('Hamed', 'Ali', 10, null),
-    ('John', 'Sina', 11, 10),
-    ('Marc', 'Wick', 12, null),
-    ('Bukayo', 'Saka', 13, 12),
-    ('Emile', 'Smith', 14, null),
-    ('Willan', 'Saliba', 15, 14),
-    ('Ash', 'Chris', 15, 14);
+INSERT INTO employee (first_name,
+  last_name, role_id, manager_id)
+  VALUES ('Jayad', 'Arshad', 1, 1),
+  ('Usama', 'Arshad', 1, 2),
+  ('Bader', 'Munir', 2, 1),
+  ('Amina', 'Hayat', 3, 2),
+  ('Aisha', 'Rahman', 4, 1),
+  ('Sina', 'Doretska', 5, 2);
--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 //download dependicies
-
 const mysql = require("mysql2");
 const inquirer = require("inquirer");
-const { connect } = require("http2");
 
 // connection to the SQL server
 const connection = mysql.createConnection({
   host: "localhost",
-  port: 3306,
   user: "root",
-  password: "",
-  database: "employees",
+  port: 3306,
+  password: "nextS+3p",
+  database: "employeesDB",
 });
 
 connection.connect((err) => {
